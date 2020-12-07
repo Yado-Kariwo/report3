4e7e0a9e9302cd60c5c903b58440aef4956b3b4d
Hikari Yoneyama <e205743@ie.u-ryukyu.ac.jp>
2020-12-07 15:24:49 +0900

    1st commit

--- app/src/main/java/jp/ac/uryukyu/ie/e205743/LivingThing.java
+++ app/src/main/java/jp/ac/uryukyu/ie/e205743/LivingThing.java
@@ -20,15 +20,25 @@ public class LivingThing{
         this.dead = false;
         System.out.printf("%sのHPは%d。攻撃力は%dです。\n", name, hitPoint, attack);
     }
-
+    /**
+     * このオブジェクトに格納されたデータのうち、deadを返す。
+     * @return　this.dead　このオブジェクトのデータにある生物の生死判定
+     */
     public boolean isDead(){
         return this.dead;
     }
-
+    /**
+     * このオブジェクトに格納されたデータのうち、nameを返す。
+     * @return　this.name　このオブジェクトのデータにある生物の名前
+     */
     public String getName(){
         return this.name;
     }
-
+    /**
+     * 対象へ攻撃するメソッド。
+     * attackに応じて乱数でダメージを算出し、opponent.wounded()によりダメージ処理を実行。
+     * @param opponent 攻撃対象
+     */
     public void attack(LivingThing opponent){
         int damage = (int)(Math.random() * this.attack);
         if(this.hitPoint > 0){
@@ -36,7 +46,11 @@ public class LivingThing{
             opponent.wounded(damage);
         }
     }
-
+    /**
+     * 自身へ攻撃されたときのダメージ処理をするメソッド。
+     * 指定されたダメージを hitPoint から引き、死亡判定を行う。
+     * @param damage 受けたダメージ
+     */
     public void wounded(int damage){
         this.hitPoint -= damage;
         if( hitPoint <= 0 ) {
